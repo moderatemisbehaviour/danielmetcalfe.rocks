fc0eebbf0cbbe356c9337e39b3751642af600b01
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2017-04-23 19:46:56 +0100

    Fixed 'deploy.sh'.
    The FTP command in the script was putting all the files in `public_html/public_html`.

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ncftpput -u $DM_ROCKS_FTP_USERNAME -p $DM_ROCKS_FTP_PASSWORD -R ftp.danielmetcalfe.rocks public_html public_html
+ncftpput -u $DM_ROCKS_FTP_USERNAME -p $DM_ROCKS_FTP_PASSWORD -R ftp.danielmetcalfe.rocks /public_html public_html/*
